cf5d72db5ed3cfacdc1e52fa7e2936d319861a6e
Basia <Basia@DESKTOP-RC24UJV>
2022-02-26 22:22:24 +0100

    First commit

---
+++ src/main/java/goodpack/barcodecalc/HelloController.java
@@ -0,0 +1,126 @@
+package goodpack.barcodecalc;
+//fx:controller="goodpack.barcodecalc.HelloController">
+
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+
+
+public class HelloController {
+    public MenuButton FourthBar;
+    public CheckBox chAllow;
+
+    public Button Black4;
+    public Button Brown4;
+    public Button Red4;
+    public Button Orange4;
+    public Button Yellow4;
+    public Button Green4;
+    public Button Blue4;
+    public Button Purple4;
+    public Button Gray4;
+    public Button White4;
+
+    public Button Black3;
+    public Button Brown3;
+    public Button Red3;
+    public Button Orange3;
+    public Button Yellow3;
+    public Button Green3;
+    public Button Blue3;
+    public Button Purple3;
+    public Button Gray3;
+    public Button White3;
+
+    public Button Black2;
+    public Button Brown2;
+    public Button Red2;
+    public Button Orange2;
+    public Button Yellow2;
+    public Button Green2;
+    public Button Blue2;
+    public Button Purple2;
+    public Button Gray2;
+    public Button White2;
+
+    public Button Black1;
+    public Button Brown1;
+    public Button Red1;
+    public Button Orange1;
+    public Button Yellow1;
+    public Button Green1;
+    public Button Blue1;
+    public Button Purple1;
+    public Button Gray1;
+    public Button White1;
+
+    public static void ThirdBarAllow(){
+        System.out.println("sada");
+
+    }
+
+
+    public void chAllowChange(){
+
+        if(chAllow.isSelected())
+        {
+            FourthBar.setDisable(false);
+        }
+        else{
+            FourthBar.setDisable(true);
+        }
+    }
+
+    public void btnBlack4(){System.out.println("Black4");}
+    public void btnBrown4(){System.out.println("Brown4");}
+    public void btnRed4(){System.out.println("btnRed4");}
+    public void btnOrange4(){System.out.println("btnOrange4");}
+    public void btnYellow4(){System.out.println("btnYellow4");}
+    public void btnGreen4(){System.out.println("btnGreen4");}
+    public void btnBlue4(){System.out.println("btnBlue4");}
+    public void btnPurple4(){System.out.println("btnPurple4");}
+    public void btnGray4(){System.out.println("btnGray4");}
+    public void btnWhite4(){System.out.println("btnWhite4");}
+////////////////////////////////////////////////////////////////
+    public void btnBlack3(){System.out.println("btnBlack3");}
+    public void btnBrown3(){System.out.println("btnBrown3");}
+    public void btnRed3(){System.out.println("btnRed3");}
+    public void btnOrange3(){System.out.println("btnOrange3");}
+    public void btnYellow3(){System.out.println("btnYellow3");}
+    public void btnGreen3(){System.out.println("btnGreen3");}
+    public void btnBlue3(){System.out.println("btnBlue3");}
+    public void btnPurple3(){System.out.println("btnPurple3");}
+    public void btnGray3(){System.out.println("btnGray3");}
+    public void btnWhite3(){System.out.println("btnWhite3");}
+////////////////////////////////////////////////////////////////
+    public void btnBlack2(){System.out.println("btnBlack2");}
+    public void btnBrown2(){System.out.println("btnBrown2");}
+    public void btnRed2(){System.out.println("btnRed2");}
+    public void btnOrange2(){System.out.println("btnOrange2");}
+    public void btnYellow2(){System.out.println("btnYellow2");}
+    public void btnGreen2(){System.out.println("btnGreen2");}
+    public void btnBlue2(){System.out.println("btnBlue2");}
+    public void btnPurple2(){System.out.println("btnPurple2");}
+    public void btnGray2(){System.out.println("btnGray2");}
+    public void btnWhite2(){System.out.println("btnWhite2");}
+////////////////////////////////////////////////////////////////
+    public void btnBlack1(){System.out.println("btnBlack1");}
+    public void btnBrown1(){System.out.println("btnBrown1");}
+    public void btnRed1(){System.out.println("btnRed1");}
+    public void btnOrange1(){System.out.println("btnOrange1");}
+    public void btnYellow1(){System.out.println("btnYellow1");}
+    public void btnGreen1(){System.out.println("btnGreen1");}
+    public void btnBlue1(){System.out.println("btnBlue1");}
+    public void btnPurple1(){System.out.println("btnPurple1");}
+    public void btnGray1(){System.out.println("btnGray1");}
+    public void btnWhite1(){System.out.println("btnWhite1");}
+
+
+
+//    @Override
+//    public void start(Stage stage) throws Exception {
+//
+//    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module goodpack.barcodecalc {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens goodpack.barcodecalc to javafx.fxml;
+    exports goodpack.barcodecalc;
+}
